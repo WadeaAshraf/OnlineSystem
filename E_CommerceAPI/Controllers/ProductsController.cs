ac7da6410de49b04320074d01ad4a44f0d5f8eff
wadea ashraf <wadea.ashraf474@gmail.com>
2024-07-01 13:40:54 +0300

    Updating to .Net 8

--- E_CommerceAPI/Controllers/ProductsController.cs
+++ E_CommerceAPI/Controllers/ProductsController.cs
@@ -4,7 +4,6 @@ using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 using System.Threading;
 
 namespace E_CommerceAPI.Controllers
--- SharedDTO/ProductsDTO.cs
+++ SharedDTO/ProductsDTO.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
+﻿
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
